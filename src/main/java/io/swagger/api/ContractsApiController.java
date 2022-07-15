8c1c02c2ec435da2538af726b0213949562e987b
PatateHunter <ichtitski@gmail.com>
2022-07-15 13:39:00 +0200

    implem kafka

--- src/main/java/io/swagger/api/ContractsApiController.java
+++ src/main/java/io/swagger/api/ContractsApiController.java
@@ -1,40 +1,25 @@
 package io.swagger.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.model.ContractActionRequest;
 import io.swagger.model.ContractRequest;
 import io.swagger.model.ContractResponse;
 import io.swagger.model.ContractsResponse;
-import io.swagger.model.ErrorResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.*;
-import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-07-15T08:30:51.850Z[GMT]")
 @RestController
@@ -45,6 +30,8 @@ public class ContractsApiController implements ContractsApi {
     private final ObjectMapper objectMapper;
 
     private final HttpServletRequest request;
+
+
 
     @org.springframework.beans.factory.annotation.Autowired
     public ContractsApiController(ObjectMapper objectMapper, HttpServletRequest request) {
